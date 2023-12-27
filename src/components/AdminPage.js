13ce199a57ca5fff7268f93caca0b280cb2919f1
yamashita-takahiro-44 <shimolen@gmail.com>
2023-12-27 12:18:13 +0900

    admin追記

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button, Input } from 'antd';
+import { Card, Button, Input, Modal } from 'antd';
 
 const AdminPage = () => {
   const [images, setImages] = useState([]);
   const [token, setToken] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedImageId, setSelectedImageId] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  // トークンを検証する関数
+  const verifyToken = () => {
+    fetch('https://shogikaisetukun.fly.dev/api/verify_token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ token })
+    })
+    .then(response => {
+      if (response.ok) {
+        setIsAuthenticated(true);
+        fetchImages();
+      } else {
+        alert('トークンが無効です');
+      }
+    })
+    .catch(error => {
+      console.error('トークンの検証に失敗しました:', error);
+    });
+  };
 
   // 画像データを取得する関数
   const fetchImages = () => {
@@ -18,30 +41,6 @@ const AdminPage = () => {
         }
       })
       .catch(error => console.error('画像の読み込みに失敗しました:', error));
-  };
-
-  // コンポーネントのマウント時に画像データを取得
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
-  // トークンを検証する関数
-  const verifyToken = () => {
-    fetch('https://shogikaisetukun.fly.dev/api/verify_token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ token })
-    })
-    .then(response => {
-      if (response.ok) {
-        setIsAuthenticated(true);
-      } else {
-        alert('トークンが無効です');
-      }
-    })
-    .catch(error => {
-      console.error('トークンの検証に失敗しました:', error);
-    });
   };
 
   // 画像を削除する関数
@@ -60,6 +59,35 @@ const AdminPage = () => {
     .catch(error => console.error('画像の削除に失敗しました:', error));
   };
 
+  // コメントデータを取得する関数
+  const fetchComments = (imageId) => {
+    fetch(`https://shogikaisetukun.fly.dev/api/images/${imageId}/comments`)
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          setComments(data);
+        } else {
+          console.error('予期せぬデータ形式:', data);
+        }
+      })
+      .catch(error => console.error('コメントの読み込みに失敗しました:', error));
+  };
+
+  // モーダルを開く関数
+  const showModal = (imageId) => {
+    setSelectedImageId(imageId);
+    fetchComments(imageId);
+    setIsModalVisible(true);
+  };
+
+  // モーダルを閉じる関数
+  const handleCancel = () => {
+    setIsModalVisible(false);
+    setSelectedImageId(null);
+    setComments([]);
+  };
+
+  // 画像とトークン入力フォームの表示
   return (
     <div>
       {!isAuthenticated && (
@@ -73,12 +101,24 @@ const AdminPage = () => {
           <Button onClick={verifyToken}>ログイン</Button>
         </div>
       )}
+
       {isAuthenticated && images.map(image => (
         <Card key={image.id}>
-          <img alt="example" src={image.url} style={{ width: '100%', height: 'auto' }} />
+          <img alt="example" src={image.url} onClick={() => showModal(image.id)} style={{ width: '100%', height: 'auto' }} />
           <Button onClick={() => handleDelete(image.id)}>削除</Button>
         </Card>
       ))}
+
+      <Modal
+        title="コメント"
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        {comments.map(comment => (
+          <p key={comment.id}>{comment.content}</p>
+        ))}
+      </Modal>
     </div>
   );
 };
